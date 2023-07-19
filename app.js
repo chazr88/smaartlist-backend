d7f146502368807e7f9d0628a716fc53f507aaae
CHAZ ROWE <chazr_88@hotmail.com>
2023-07-18 21:50:48 -0400

    first push

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+const createSchema = require('./config/schema');
+const listRoutes = require('./routes/listsRoutes.js');
+
+const app = express();
+
+// Call the createSchema function to create the schema
+// createSchema();
+
+// Middleware to enable CORS
+app.use(cors());
+
+// Middleware to parse request bodies as JSON
+app.use(express.json());
+
+// Set up routes
+app.use('/lists', listRoutes);
+
+// Start the server
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
+});
