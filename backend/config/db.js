24b8db40c78c6cd09331358febbb1bf5354787aa
CHAZ ROWE <chazr_88@hotmail.com>
2023-10-01 13:22:30 -0400

    Fixed backend

--- backend/config/db.js
+++
@@ -1,7 +0,0 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString:"postgres://admin:scx2lLagzTwORdJqsTdcxvVHQqP0H5IN@dpg-cinl9stph6ei90fatong-a/smart_list"
-});
-
-module.exports = pool;
